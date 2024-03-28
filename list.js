3d44fd4b48a58ab3b89f2ad4c470da9c96986700
JYEEEE99 <attjyh7@naver.com>
2024-03-28 15:56:44 +0900

    commit

--- list.js
+++ list.js
@@ -1,20 +1,59 @@
 var products = [
-    { id : 0, price : 70000, title : 'Blossom Dress' },
-    { id : 1, price : 50000, title : 'Springfield Shirt' },
-    { id : 2, price : 60000, title : 'Black Monastery' }
-  ];
-  var title = $('.card-title')
-  var price = $('.car-price')
-  function titleChange() {
-    for(i=0; i < 3; i++){
-        title.eq(i).html(products[i].title)
-    }
+  { id: 0, price: 70000, title: "Blossom Dress" },
+  { id: 1, price: 50000, title: "Springfield Shirt" },
+  { id: 2, price: 60000, title: "Black Monastery" },
+];
+function layout() {
+  for (i = 0; i < 3; i++) {
+    $(".row").append(`<div class="col-sm-4">
+        <img src="https://via.placeholder.com/600" class="w-100">
+        <h5>${products[i].title}</h5>
+        <p>가격 : ${products[i].price}</p>
+      </div>`);
   }
-  function priceChange() {
-    for(i=0; i < 3; i++) {
-        // price.eq(i).html('가격: ' + products[i].price)
-        price.eq(i).html(`가격: ${products[i].price}`)
+}
+layout();
+
+function layout2() {
+  $.get("https://codingapple1.github.io/js/more1.json").done((data) => {
+    data.forEach((a, i) => {
+      $(".row").append(`<div class="col-sm-4">
+        <img src="https://via.placeholder.com/600" class="w-100">
+        <h5>${data[i].title}</h5>
+        <p>가격 : ${data[i].price}</p>
+      </div>`);
+    });
+  });
+}
+function layout3() {
+  $.get("https://codingapple1.github.io/js/more2.json").done((data) => {
+    data.forEach((a, i) => {
+      $(".row").append(`<div class="col-sm-4">
+        <img src="https://via.placeholder.com/600" class="w-100">
+        <h5>${data[i].title}</h5>
+        <p>가격 : ${data[i].price}</p>
+      </div>`);
+    });
+  });
+}
+var count = 0;
+function moreBtn() {
+  $(".more-btn").on("click", function () {
+    count++;
+    if (count == 1) {
+      layout2();
+    } else if (count == 2) {
+      layout3();
     }
-  } 
-  priceChange()
-  titleChange()
+  });
+}
+moreBtn();
+$("#price").on("click", function () {
+  products.sort(function (a, b) {
+    return a.price - b.price;
+  });
+  $(".row").html("");
+  layout();
+  moreBtn();
+});
+$("#name").on("click", function () {});
